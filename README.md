68fe584e0610cf47032e59996e822590c98631dc
sebastien foucault <sebastien.foucault@gmail.com>
2014-08-02 15:11:26 +0200

    More unit test

--- README.md
+++ README.md
@@ -2,9 +2,6 @@ dmessify.py
 ===========
 This Python script allows to organize photos based on EXIF metadata.
 
-* It manages the target directory using template 
-* It manages duplicate files
-
 Python version
 ==============
 
--- tests/testmapper.py
+++ tests/testmapper.py
@@ -4,6 +4,7 @@ import testutils
 import os
 import unittest
 
+from time import gmtime
 
 class TestMapper(unittest.TestCase):
 
@@ -29,10 +30,33 @@ class TestMapper(unittest.TestCase):
         rel_result = os.path.relpath(result, self._output_directory)
         self.assertEqual("2008/12/2008-12-06.14.11.06-1.JPG", rel_result)
 
+    def test_no_exif_mapping(self):
+        options = self._create_default_options()
+        path_mapper = mapper.ImagePathMapper(options)
+
+        input = testutils.resource("resources/mapper/IMG-02.JPG")
+        result = path_mapper.map(input)
+
+        sec = os.path.getctime(input)
+        time = gmtime(sec)
+        fields = {
+            'year': "{:04}".format(time.tm_year),
+            'month': "{:02}".format(time.tm_mon),
+            'day': "{:02}".format(time.tm_mday),
+            'hour': "{:02}".format(time.tm_hour),
+            'minute': "{:02}".format(time.tm_min),
+            'second': "{:02}".format(time.tm_sec)
+        }
+
+        rel_result = os.path.relpath(result, self._output_directory)
+        self.assertEqual("{}/{}/{}-{}-{}.{}.{}.{}.JPG".format(
+            fields['year'], fields['month'],
+            fields['year'], fields['month'], fields['day'],
+            fields['hour'], fields['minute'], fields['second']), rel_result)
 
     def _create_default_options(self):
         options = dict()
-        options['dir.target'] = self._output_directory;
+        options['dir.target'] = self._output_directory
         options['dir.unmanaged'] = os.path.join(self._output_directory, "unmanaged")
         options['dir.template'] = "$year/$month"
         options['file.template'] = '$year-$month-$day.$hour.$minute.$second.$extension'
